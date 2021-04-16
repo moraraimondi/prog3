eeab17d43faf7d25de5b2863564ef747f04219ab
lolamarotta <56725439+lolamarotta@users.noreply.github.com>
2021-04-16 11:32:31 -0300

    api y tarjetas

--- proyecto-integrador3/src/App.js
+++ proyecto-integrador3/src/App.js
@@ -1,5 +1,14 @@
 import Header from './components/Header';
 
+// componentDidMount() {
+//   fetch("https://randomuser.me/api/?inc=name,login&results=12")
+//   .then(resource => resource.json())
+//   .then(data => {
+//       this.setState({items: data.results});
+//       console.log(this.state.items);
+//   })
+// }; 
+
 function App() {
   return (
     <body>
---
+++ proyecto-integrador3/src/components/Tarjetas.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+// class Tarjetas extends Component {
+//     constructor(props){
+//         super(porps);
+//         this.state = {
+//             color: this.props.color
+//         }
+//     }
+
+//     CambiarColor = (nuevoColor) => {
+//         console.log(nuevoColor);
+//         if(this.state.color == this.state.color)
+//     }
+// }
+
