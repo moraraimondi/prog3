4a6237879f2027d2025020257ba417fa124662a5
lolamarotta <56725439+lolamarotta@users.noreply.github.com>
2021-04-22 22:54:10 -0300

    js agregar tarjetas creo q hecho, pero salta error cuando lo uno a app.js

--- proyecto-integrador3/src/components/AgregarTarjetas.js
+++ proyecto-integrador3/src/components/AgregarTarjetas.js
@@ -2,32 +2,54 @@ import React, {Component} from "react";
 
 class AgregarTarjetas extends Component {
 
-    componentDidMount() {
-        fetch("https://randomuser.me/api/?results=12")  
-        .then(res => 
-          res.json())
-        .then(
-          (data) => {
-            console.log(data);
-    
-              this.setState({
-                isLoaded: true,
-                items: data.results,
-              });
-            },
-            (error) => {
-              this.setState({
-                isLoaded: true,
-                error
-              });
-            }
-          )
-      }
+  addTarjeta(e){
+    e.preventDefault();
+    var x = document.getElementById("formulario");
+    if(x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
 
-      addTarjetas() {
-          
-      }
+  handleChange(event){
+    this.setState({value: event.target.value});
+  }
+  
+  handleSubmit(event){
+    alert('Agregaste:' + this.state.value);
+    event.preventDefault();
+  }
 
+  render(){
+    const {error, isloaded, items} = this.state;
+    if(error){
+      return <div>Hubo un error: {error.message}</div>;
+    }
+    else if (!isloaded) {
+      return <div>Cargando...</div>;
+    }
+    else {
+      return (
+        <React.Fragment>
+
+        <div>
+
+        <button onclick={this.addTarjeta.bind(this)}>Agregar tarjetas</button>
+
+        <form id="formulario">
+          <label>¿Cuántas tarjetas queres agregar?</label>
+          <input type="number"></input>
+        </form>
+
+        </div>
+
+        </React.Fragment>
+
+      )
+        
+    }
+  }
 };
 
 export default AgregarTarjetas;
