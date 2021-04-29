14897384a18dbd4743185d7a502dd3c4a8a1f943
tomasgiamp99 <tgiampaoletti@udesa.edu.ar>
2021-04-29 18:46:33 -0300

    up

--- proyecto-integrador3/src/components/Contenedor.js
+++ proyecto-integrador3/src/components/Contenedor.js
@@ -1,172 +1,168 @@
-import React, {Component} from "react";
-import Tarjetas from './Tarjetas';
-// import Filtros from './Filtros';
+  import React, {Component} from "react";
+  import Tarjetas from './Tarjetas';
+  // import Filtros from './Filtros';
 
-class Contenedor extends Component {
-    constructor(props) {
-        super(props);
+  class Contenedor extends Component {
+      constructor(props) {
+          super(props);
 
-        this.state = {
-          error: null,
-          isLoaded: false,
-          numero: "",
-          items: [],
-          nombre:"",
-          // sexo:"",
-          // edad:"",
-          // apellido:""
-        };
-      }
-
-      componentDidMount() {
-        fetch("https://randomuser.me/api/?results=12")  
-        .then(res => 
-          res.json())
-        .then(
-          (data) => {
-            console.log(data);
-    
-              this.setState({
-                isLoaded: true,
-                items: data.results,
-              });
-
-
-            },
-        (error) => {
-              this.setState({
-                isLoaded: true,
-                error
-              });
-            }
-          )
-      }
+          this.state = {
+            error: null,
+            isLoaded: false,
+            numero: "",
+            items: []
+          };
+        }
+        
+        componentDidMount() {
+          fetch("https://randomuser.me/api/?results=12")  
+          .then(res => 
+            res.json())
+          .then(
+            (data) => {
+              console.log(data);
       
-      filtrarTarjetas(){
-        let dataAfiltrar = document.getElementById("inputDataFiltro").value
-        let campoAfiltrar = document.getElementById("selectDataFiltro").value
-
-        console.log(dataAfiltrar)
-        console.log(campoAfiltrar)
+                this.setState({
+                  isLoaded: true,
+                  items: data.results,
+                });
 
 
-        if (campoAfiltrar === "Edad"){
-          let resultado = this.state.items.filter( (item) => {
-            return item.dob.age == dataAfiltrar 
-          }) 
-          this.setState({items: resultado})
-        } else if (campoAfiltrar === "Nombre"){
-          let resultado = this.state.items.filter( (item) => {
-            return item.name.first.includes(dataAfiltrar)
+              },
+          (error) => {
+                this.setState({
+                  isLoaded: true,
+                  error
+                });
+              }
+            )
+        }
+        
+        filtrarTarjetas(){
+          let dataAfiltrar = document.getElementById("inputDataFiltro").value
+          let campoAfiltrar = document.getElementById("selectDataFiltro").value
+
+          console.log(dataAfiltrar)
+          console.log(campoAfiltrar)
+
+
+          if (campoAfiltrar === "Edad"){
+            let resultado = this.state.items.filter( (item) => {
+              return item.dob.age == dataAfiltrar 
+            }) 
+            this.setState({items: resultado})
+          } else if (campoAfiltrar === "Nombre"){
+            let resultado = this.state.items.filter( (item) => {
+              return item.name.first.includes(dataAfiltrar)
+            })
+            this.setState({items: resultado})
+          } else if (campoAfiltrar === "Sexo"){
+            let resultado = this.state.items.filter( (item) => {
+              return item.gender === dataAfiltrar
+            })  
+            this.setState({items:resultado})
+          }
+        }
+
+
+        borrarTarjeta(idItem){
+          let resultado = this.state.items.filter((item)=>{
+            return item.login.uuid!== idItem
           })
           this.setState({items: resultado})
-        } else if (campoAfiltrar === "Sexo"){
-          let resultado = this.state.items.filter( (item) => {
-            return item.gender === dataAfiltrar
-          })  
-          this.setState({items:resultado})
         }
-      }
 
+        abrirFormulario(){
+          var x = document.getElementById("formulario");
+          if (x.style.display === "none") {
+            x.style.display = "block";
+          } else {
+            x.style.display = "none";
+          }
+            console.log(x.style.display)
+        }
 
-      borrarTarjeta(idItem){
-        let resultado = this.state.items.filter((item)=>{
-          return item.login.uuid!== idItem
+        agregarTarjetas(){
+          fetch("https://randomuser.me/api/?results="+ this.state.numero )
+        .then(resource => resource.json())
+        .then(data => {
+          console.log(data)
+          let add = this.state.items.concat(data.results);
+          this.setState({items: add});
         })
-        this.setState({items: resultado})
       }
 
-      abrirFormulario(){
-        var x = document.getElementById("formulario");
-         if (x.style.display === "none") {
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
-        }
-          console.log(x.style.display)
-      }
+      render(){
+          const {error, isLoaded, items} = this.state;
 
-      agregarTarjetas(){
-        fetch("https://randomuser.me/api/?results="+ this.state.numero )
-      .then(resource => resource.json())
-      .then(data => {
-        console.log(data)
-        let add = this.state.items.concat(data.results);
-        this.setState({items: add});
-      })
-    }
+    if(error){
+      return <div> Error: {error.message}</div>;
+    } else {
+      return (
 
-    render(){
-        const {error, isLoaded, items} = this.state;
+          <div className="container">
 
-  if(error){
-    return <div> Error: {error.message}</div>;
-  } else {
-    return (
+              {/*SECCIÓN IZQUIERDA*/}
 
-        <div className="container">
+              <div className="left-section">
 
-            {/*SECCIÓN IZQUIERDA*/}
+                <div className="opciones">
 
-            <div className="left-section">
+                  {/* FILTROS */}
 
-              <div className="opciones">
+                  <div className="filtros-div">
 
-                {/* FILTROS */}
+                    <h2 className="filtros-title">Filtros</h2>
+                    
+                    <label style={{fontWeight:"600", color:"424242"}}>Filtrar por</label>
+                    <select id="selectDataFiltro">
+                      <option>Nombre</option>
+                      <option>Edad</option>
+                      <option>Sexo</option>
+                    </select>
 
-                <div className="filtros-div">
+                    <input className="inputDataFiltro" name="filtroData" id="inputDataFiltro"/>
 
-                  <h2 className="filtros-title">Filtros</h2>
-                  
-                  <label style={{fontWeight:"600", color:"424242"}}>Filtrar por</label>
-                  <select id="selectDataFiltro">
-                    <option>Nombre</option>
-                    <option>Edad</option>
-                    <option>Sexo</option>
-                  </select>
+                    <div className="div-botones">
+                      <button className="boton" onClick={this.filtrarTarjetas.bind(this)}>Filtrar</button>
+                      <button className="boton-secundario boton"onClick={this.componentDidMount.bind(this)}>Reset</button>
+                    </div>
 
-                  <input className="inputDataFiltro" name="filtroData" id="inputDataFiltro"/>
-
-                  <div className="div-botones">
-                    <button className="boton" onClick={this.filtrarTarjetas.bind(this)}>Filtrar</button>
-                    <button className="boton-secundario boton"onClick={this.componentDidMount.bind(this)}>Reset</button>
                   </div>
 
-                </div>
+                  {/* AGREGAR TARJETAS */}
 
-                {/* AGREGAR TARJETAS */}
-
-                <div className="formulario-agregar">
-                    <button className="boton boton-agregar" onClick={this.abrirFormulario.bind(this)}>Agregar tarjetas</button>
-                    <div id="formulario" style={{display:"none"}}>
-                      <p>¿Cuántas tarjetas queres agregar?</p>
-                      <input type="number" onChange={(event) => this.setState({numero: event.target.value})}></input>
-                      <button className="boton" onClick={this.agregarTarjetas.bind(this)}> Agregar </button>  
-                    </div>
+                  <div className="formulario-agregar">
+                      <button className="boton boton-agregar" onClick={this.abrirFormulario.bind(this)}>Agregar tarjetas</button>
+                      <div id="formulario" style={{display:"none"}}>
+                        <p>¿Cuántas tarjetas queres agregar?</p>
+                        <input type="number" onChange={(event) => this.setState({numero: event.target.value})}></input>
+                        <button className="boton" onClick={this.agregarTarjetas.bind(this)}> Agregar </button>  
+                      </div>
+                  </div>
                 </div>
+                
               </div>
-              
-            </div>
 
-            {/*SECCIÓN DERECHA */}
+              {/*SECCIÓN DERECHA */}
 
-            <div className="right-section">
+              <div className="right-section">
 
-                {/* Contenedor de tarjetas */}
-                <div className="tarjetas-container">
-                    {items.map(item=> (
-                    
-                    <Tarjetas nombre={item.name.first} apellido={item.name.last} mail={item.email} 
-                    fecha={item.dob.date} edad={item.dob.age} foto={item.picture.large} 
-                    id={item.login.uuid} borrarTarjeta= {this.borrarTarjeta.bind(this)}/>
+                  {/* Contenedor de tarjetas */}
+                  <div className="tarjetas-container">
+                      {items.map(item=> (
+                      
+                      <Tarjetas nombre={item.name.first} apellido={item.name.last} mail={item.email} 
+                      fecha={item.dob.date} edad={item.dob.age} foto={item.picture.large} 
+                      id={item.login.uuid} borrarTarjeta= {this.borrarTarjeta.bind(this)}/>
 
-                    ))}
-                </div>
-            <div/>
-        </div>
-        </div>
-    )
-    }
-}}
+                      ))}
+                  </div>
+              <div/>
+          </div>
+          </div>
+      )
+      }
+  }}
 
-export default Contenedor;
+  export default Contenedor;
